4bb8b3ce62c98936a3b6aa92b37bb07a4bd72cbe
mackenziestein <mackenziestein415@gmail.com>
2016-12-11 12:56:01 -0500

    added registers and muxes for register file and ALU

--- DataPath.sv
+++ DataPath.sv
@@ -15,16 +15,16 @@ module DataPath(clock, pcQ, instr, pcD, regWriteEnable);
    logic [0:0] 	       PCWrite, IorD, IRWrite, ALUSrcA, ALUSrcB;
    logic [4:0] 	       ALUControl;
    // memory
-   logic [31:0]        instA, ALUResult, dataA, WD;
+   logic [31:0]        instA, ALUResult, dataA, WD, instrFromMem;
    logic [0:0] 	       WE;
    // register file
    logic [4:0] 	       A3, A2, A1, RsOrRt, A3assign, r7default;
    logic 	       WE3, clk;
-   logic [31:0]        WD3, RD1, RD2  RD;
+   logic [31:0]        WD3, RD1, RD2  RD, dataOut;
    logic [31:0]        SignImm, signImm22, pc4AdderIn, branchAdderOut, PCBranch;
    logic [1:0] 	       constant0;
    // ALU
-   logic [31:0]        SrcA, SrcB, muxSrcBin, Result, muxBranchOut, ALUOut;
+   logic [31:0]        SrcA, SrcB, muxSrcBin, Result, muxBranchOut, ALUOut, RDA, RDB, SrcAIn, SrcBIn;
    // jump and branch
    logic [31:0]        PCJump, jumpInst, PCNext, PCJumpReg;
 
@@ -44,35 +44,35 @@ module DataPath(clock, pcQ, instr, pcD, regWriteEnable);
    assign pcPlus4 = adderOut;
 
    // CONTROL UNIT
- 
-   
-   Control theControl(instr, memToReg, memWrite, branchEnable, ALUControl, ALUSrc, regDst, regWriteEnable, jump, jumpReg, PCWrite, IorO, IRWrite, ALUSrcA, ALUSrcB, alu4, alu3, alu2, alu1, alu0);
+    
+   Control theControl(instr, memToReg, memWrite, branchEnable, ALUControl, ALUSrc, regDst, regWriteEnable, jump, jumpReg, PCWrite, IorD, IRWrite, ALUSrcA, ALUSrcB, alu4, alu3, alu2, alu1, alu0);
    
    // INSTRUCTION AND DATA MEMORY
-   // input logic addr, writeData, clk, writeEnable
-   // output logic read
- 
-
-   assign dataA = ALUResult;
-   assign instA = pcQ; // this needs to be changed - either the output from the ALU or the output from the PC register
    
+   assign dataA = ALUResult;
+   mux4to1B32 memoryIn(1'b0, IorD, 32'b0, 32'b0, ALUOut, pcQ, instA);
+   combinedMemroy idmem(instA, instrFromMem, WD, clk, WE); 
+   
+   //assign instA = pcQ; // this needs to be changed - either the output from the ALU or the output from the PC register
    // dataMemory data(dataA, RD, WD, clk, WE);
    // instructionMemory imem(instA,instr);
 
-   mux4to1B32 memoryIn(1'b0, IorD, 32'b0, 32'b0, ALUOut, pcQ, insta);
-   
-   combinedMemroy idmem(instA, instr, WD, clk, WE); 
    
    //REGISTER FILE 
 
    registerFile theRegisters(A1,A2, A3, clk, WE3, WD3, RD1, RD2);
 
+       // new things
+   enabledRegister instructionIn(instrFromMem, instr, clock, IRWrite);
+   enabledRegister instructionIn(instrFromMem, dataOut, clock, 1'b1);
+   
+       // old things
    assign r7default = 5'b11111;
    
    mux2to1B5 muxA3(regDst, instr[15:11], instr[20:16], RsOrRt);
    mux2to1B5 muxJal(jump, r7default, RsOrRt, A3assign); 
    
-   assign clk = clock;
+   assign clk = clock; // WHY DO WE DO THIS? WHY NOT JUST USE CLOCK?
    assign A1 = instr[25:21];
    assign A3 = A3assign;  // A3 is either 20:16 or 15:11, based on RegDst
    assign A2 = instr[20:16];
@@ -80,32 +80,39 @@ module DataPath(clock, pcQ, instr, pcD, regWriteEnable);
    
    assign constant0 = 2'b0;
    assign SignImm = {{16{instr[15]}}, instr[15:0]};
+   
+   //ALU THINGS
 
-   //SOME BRANCH THINGS
-   
-   adder branchAdd(signImm22, pc4AdderIn, branchAdderOut);
-   
-   assign signImm22 = {SignImm[29:0], constant0};
-   assign pc4AdderIn = pcPlus4;
-   assign PCBranch = branchAdderOut;
-   
-  //ALU THINGS
-   
-   mux4to1B32 muxRD2(1'b0, ALUSrc, 32'b0, 32'b0, SignImm, RD2, muxSrcBin);
+   enabledRegister RD1Out(RD1, RDA, clock, 1'b1);
+   enabledRegister RD2Out(RD2, RDB, clock, 1'b1);
 
-   assign SrcB = muxSrcBin;
-   assign SrcA = RD1;
+   mux4to1B32 ALUA(1'b0, ALUSrcA, 32'b0, 32'b0, RDA, pcQ, SrcAIn);
+   mux4to1B32 ALUB(1'b0, ALUSrcB, 32'b0, 32'b0, SignImm, constant4, SrcBIn);
+   
+   
+   //mux4to1B32 muxRD2(1'b0, ALUSrc, 32'b0, 32'b0, SignImm, RD2, muxSrcBin);
+   
+   assign SrcA = SrcAIn;
+   assign SrcB = SrcBIn;
 
    ALU theALU(SrcA, SrcB, ALUControl, ALUResult);    
 
    mux4to1B32 muxRD(jump, memToReg, 32'b0, pcPlus4, RD, ALUResult, Result);
 
-   assign WD3 = Result;
-   assign WD = RD2;
+   assign WD3 = dataOut; // dataOut assigned in register file section based on instrFromMem
+   assign WD = RDB;
    assign WE = memWrite;
 
    enabledRegister(ALUResult, ALUOut, clock, 1'b1);
+
+			  
+   //SOME BRANCH THINGS
    
+   adder branchAdd(signImm22, pc4AdderIn, branchAdderOut);
+   
+   assign signImm22 = {SignImm[29:0], constant0}; // ??? what is this used for
+   assign pc4AdderIn = pcPlus4;
+   assign PCBranch = branchAdderOut;
 
    //PC THINGS
 
