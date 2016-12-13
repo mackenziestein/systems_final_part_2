47282f87c49a3efc74f1d14d0c2e196cbbdaa4a3
mackenziestein <mackenziestein415@gmail.com>
2016-12-12 23:37:21 -0500

    added comments to and cleaned up control

--- Control.sv
+++ Control.sv
@@ -11,15 +11,29 @@ module Control(clock, ins, memToReg, memWrite, branchEnable, ALUControl, ALUSrc,
    logic [31:0] lw32, sw32, lw1out, sw1out;
 
    assign andr = ins[31] & ~ins[30] & ~ins[29] & ~ins[28] & ~ins[27] & ~ins[26];
+   
+   // lw enable lines:
    assign lw = ins[31] & ~ins[30] & ~ins[29] & ~ins[28] & ins[27] & ins[26] & ~sw2 & ~lw2;
+   // lw32 is 32-bit version of lw, with all 0's except least significant bit.  used for register.
    assign lw32 = {31'b0, lw};
+   // lwen is high if either lw or lw2 is high, indicating that the value of lw2 should be
+   // updated on both the first and second clock ticks of a lw instruction
    assign lwen = lw | lw2;
+   // register to save the value of lw2 (in 32-bit form)
    enabledRegister lwSave(lw32, lw1out, clock, lwen);
+   // take 32-bit version of lw2 and use only least significant bit
    assign lw2 = lw1out[0];
+
+   // sw enable lines:
    assign sw = ins[31] & ~ins[30] & ins[29] & ~ins[28] & ins[27] & ins[26] & ~lw2 & ~sw2;
+   // sw32 is 32-bit version of sw, with all 0's except least significant bit. used for register.
    assign sw32 = {31'b0, sw};
+   // swen is high if either sw or sw2 is high, indicating that the value of sw2 shoudl be
+   // updated on both the first and second clock ticks of a sw instruction
    assign swen = sw | sw2;
+   // register to save the value of sw2 (in 32-bit form)
    enabledRegister swSave(sw32, sw1out, clock, swen);
+   // take 32-bit version of sw2 and use only least significant bit
    assign sw2 = sw1out[0];
    
    assign jr = ~ins[31] & ~ins[30] & ins[29] & ~ins[28] & ~ins[27] & ~ins[26];
@@ -47,13 +61,11 @@ module Control(clock, ins, memToReg, memWrite, branchEnable, ALUControl, ALUSrc,
    assign jump = jr | jal;
    assign jumpReg = jr;
    
-   //assign PCWrite = ~((lw & ~lw2) | (sw & ~sw2));
    assign PCWrite = ~(lw | sw);
    assign IorD = lw2 | sw2;
    assign IRWrite = lw | sw;
-   //~(lw2 | sw2);
-   assign ALUSrcA = 1'b1;
-   // nori | lw | sw | lw2 | sw2;
+   assign ALUSrcA = 1'b1; // always high - don't ever want to choose pcQ for
+   // source A because we are not computing the PC+4 in the ALU. use external adder.
    assign srcB1 = lw | sw | lw2 | sw2 |  nori | jump;
    assign srcB0 = jump;
    assign ALUSrcB = {srcB1, srcB0};
